9cd0832a461e159c26ebcb2c3d700634a1c6d0a0
Anthony Frizalone <afrizalone@me.com>
2020-11-19 02:00:30 -0500

    Initial Commit

---
+++ MedlyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedlyApp
+//
+//  Created by Anthony Frizalone on 11/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
